01355a324c05dd7e121a86fb2f386e979325dc65
rlarudgk <flyfalls@naver.com>
2022-08-16 15:18:35 +0900

    배열

---
+++ src/Chapter5/ArraySample.java
@@ -0,0 +1,26 @@
+package Chapter5;
+
+public class ArraySample {
+	
+	public static void main(String[] args) {
+		
+		String[] names = new String[25]; // 25개짜리 데이터를 저장 할 수 있는 배열
+		
+		names[0]="박형준";
+	    names[1]="김서빈";
+	    names[3]="이찬영";
+	    names[2]="민경진";
+	    names[4]="허근주";
+	    names[5]="유나래";
+	    names[6]="박시윤";
+	    
+	    int[] scores = new int[3];
+	    scores[0] =100;
+	    scores[1] =85;
+	    scores[2] =55;
+	    
+		
+		
+	}
+
+}
---
+++ src/Chapter5/ArraySample5.java
@@ -0,0 +1,19 @@
+package Chapter5;
+
+public class ArraySample5 {
+	public static void main(String[] args) {
+		
+		int[] scores = {95,71,84,93,87};
+		
+		int sum = 0;
+		
+		for (int score : scores) {
+			sum = sum + score;
+		}
+		System.out.println("점수 총합 = " + sum);
+		
+		double avg =(double)sum / scores.length;
+		System.out.println("점수 평균 = " + avg);
+	}
+
+}
