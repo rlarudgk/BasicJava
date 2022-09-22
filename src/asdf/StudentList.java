053017a1b66a90a794565d7664a7d40b119bda1c
rlarudgk <flyfalls@naver.com>
2022-09-23 00:44:40 +0900

    Student List

---
+++ src/asdf/StudentList.java
@@ -0,0 +1,91 @@
+package asdf;
+
+public class StudentList implements Comparable<StudentList> {
+	
+	private String studentNumber;
+	private String name;
+	private int KorScore;
+	private int engScore;
+	private int mathScore;
+	private int score;
+	private int rank;
+	
+	public StudentList(String studentNumber, String name, int korScore, int engScore, int mathScore) {
+		
+		this.studentNumber = studentNumber;
+		this.name = name;
+		this.KorScore = korScore;
+		this.engScore = engScore;
+		this.mathScore = mathScore;
+		setScore(korScore, engScore, mathScore);
+	}
+	
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKorScore() {
+		return KorScore;
+	}
+
+	public void setKorScore(int korScore) {
+		KorScore = korScore;
+	}
+
+	public int getEngScore() {
+		return engScore;
+	}
+
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+
+	public int getMathScore() {
+		return mathScore;
+	}
+
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+
+	public int getScore() {
+		return score = score ;
+	}
+
+	public void setScore(int KorScore , int engScore , int mathScore) {
+		this.score = KorScore + engScore + mathScore;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentList [studentNumber=" + studentNumber + ", name=" + name + ", KorScore=" + KorScore
+				+ ", engScore=" + engScore + ", mathScore=" + mathScore + ", score=" + score + ", rank=" + rank + "]";
+	}
+
+	@Override
+	public int compareTo(StudentList member) {
+		
+		return this.getStudentNumber().compareTo(member.getStudentNumber());
+		
+	}
+}
---
+++ src/asdf/StudentTest.java
@@ -0,0 +1,70 @@
+package asdf;
+
+import java.util.ArrayList;
+import java.util.Collections;import java.util.Comparator;
+
+public class StudentTest{
+	public static void main(String[] args) {
+		
+		ArrayList<StudentList> list = new ArrayList<StudentList>();
+		
+		list.add(new StudentList("10001", "김경하", 100, 100, 100));
+		list.add(new StudentList("10005", "서범수", 100, 90, 95));
+		list.add(new StudentList("10003", "김소현", 90, 85, 100));
+		list.add(new StudentList("10004", "전혜연", 80, 95, 100));
+		list.add(new StudentList("10002", "김예슬", 75, 60, 45));
+		list.add(new StudentList("10006", "이정우", 95, 90, 90));
+		
+	      for(int i=0; i<list.size(); i++) {
+	          int rank=1;
+	          for(int j=0; j<list.size(); j++) {
+	                if(list.get(i).getScore() < list.get(j).getScore()) {
+	                   rank++;
+	                }
+	                list.get(i).setRank(rank);
+	          }
+	       }
+
+		System.out.println("정렬 전: "  );
+		for(StudentList mem : list ) {
+			System.out.println(mem);
+		}
+		System.out.println("--------------------------");
+		
+		Collections.sort(list);
+		System.out.println("총점의 오름차순으로 정렬 후 : ");	
+		for (StudentList mem : list) {
+			System.out.println(mem);	
+		}
+		System.out.println("--------------------------");
+	
+		Collections.sort(list, new SortNumDesc());
+		System.out.println("총점의 내림차순으로 정렬 후 : ");	
+		for (StudentList mem: list) {
+			System.out.println(mem);	
+		}
+		System.out.println("--------------------------");
+	}
+	}
+
+class SortNumDesc implements Comparator<StudentList> {
+
+	@Override
+	public int compare(StudentList mem1, StudentList mem2) {
+		if(mem1.getScore() > mem2.getScore()) {
+			return -1;
+		}else if(mem1.getScore() < mem2.getScore()) {
+			return 1;
+		}else {
+			//점수가 같으면 학번을 비교해서 내림차로
+//			if(mem1.getScore() == mem2.getScore()) {
+//				Collections.sort(list, new SortNumDesc());
+//			}
+			
+			return -1 ;
+		}
+	}
+}
+
+
+
